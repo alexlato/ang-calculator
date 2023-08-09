6fd50bcd97a4681828ad57bc73098cb144b8658a
alexlato <86857302+alexlato@users.noreply.github.com>
2023-08-08 20:17:07 -0700

    calValue, funcT, onClickValue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ang-calculator';
+
+  calValue: number = 0;
+  funcT: any = 'NoFunction';
+
+  onClickValue(val: string, type: any) {
+    console.log(val, type);
+  }
 }
